16276eeddb44a210de84debfecf8ebe29c40e51d
George Allakhverdyan <george.allakhverdyan@gmail.com>
2022-09-26 09:59:09 -0500

    fix: update to go1.17 build flag

--- common/tools/tools.go
+++ common/tools/tools.go
@@ -1,9 +1,9 @@
-// +build tools
+//go:build tools
 
 package tools
 
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "google.golang.org/protobuf/protoc-gen-go"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
